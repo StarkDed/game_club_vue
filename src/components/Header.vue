89b3dab2efc56e3ae07fddba290681983d8be729
Denis Starkov <starkden1000@gmail.com>
2025-04-26 19:13:10 +0300

    added page of rants

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
     <div class="options">
       <nav>
         <router-link to="/">Home</router-link>
-        <router-link to="">rants</router-link>
+        <router-link to="/rants">rants</router-link>
         <router-link to="">about us</router-link>
       </nav>
       <button class="signIn" @click="navigateToAuth('login')">Sign in</button>
--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "@/components/HomePage.vue";
 import AuthForm from "@/components/AuthForm.vue";
+import Rants from "@/components/Rants.vue";
 
 const routes = [
   {
@@ -11,6 +12,10 @@ const routes = [
     path: "/auth",
     component: AuthForm,
     props: (route) => ({ mode: route.query.mode || "login" }),
+  },
+  {
+    path: "/rants",
+    component: Rants,
   },
 ];
 
